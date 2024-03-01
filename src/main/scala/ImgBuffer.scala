3313a870b9a3df8a1fdd0da64961fc296a2e1c15
Lux <iansseijelly@berkeley.edu>
2024-03-01 15:31:26 -0800

    img buffer first draft

--- src/main/scala/ImgBuffer.scala
+++ src/main/scala/ImgBuffer.scala
@@ -2,37 +2,104 @@ package revelio
 
 import chisel3._
 import chisel3.util._
+import testchipip.serdes.SerialWidthSlicer
 
-class SRAMImgBuffer(val nRows: Int, val rWidth: Int, val imgWidth: Int, val imgHeight: Int) extends Module {
+class SRAMImgBuffer(val nRows: Int, val imgWidth: Int, val imgHeight: Int) extends Module {
+
+    val rWidth : Int = 8
+    val wWidth : Int = 32
+
     val io = IO(new Bundle {
         // write by rows
         val write = Flipped(Decoupled(UInt(32.W)))
         // read by columns
-        val read = Decoupled(UInt(rWidth.W))
+        val read = new Bundle {
+            val request = new Bundle {
+                val valid = Input(Bool())
+                val ready = Output(Bool())
+            }
+            val response = new Bundle {
+                val valid = Output(Bool())
+                val bits = Vec(nRows, UInt(rWidth.W))
+            }
+        }
     })
     require(rWidth % 32 == 0, "The read width must be a multiple of 32")
     val nBanks = nRows + 1 // an extra bank for the write
     val dBanks = imgWidth * 8 / rWidth // depth of each bank in terms of rWidth 
-    val b_valids = RegInit(VecInit(Seq.fill(nBanks){false.B}))
-    val w_enable = RegInit(VecInit(Seq.fill(nBanks){false.B}))
-    val r_enable = RegInit(VecInit(Seq.fill(nBanks){false.B}))
+    
+    // COARSE counter tracking the number of columns writes for this row
+    val (w_col_count, w_col_done) = Counter(io.write.fire, 0 until imgWidth by 4)
+    // enq_ptr is the pointer to the bank that is currently being written to
+    val (enq_ptr, _) = Counter(w_col_done, nBanks)
+    // counter tracking the number of rows written
+    val (w_row_count, w_row_done) = Counter(w_col_done, imgHeight)
+    val w_enable = VecInit((0 until nBanks map (i => i.U === enq_ptr && w_serdes.io.narrow.fire)).toSeq)
+    // FINE counter tracking the serial width slicer write address
+    val (w_bytes_count, w_bytes_done) = Counter(w_enable(enq_ptr), wWidth / rWidth)
+    val w_addr = Wire(UInt(log2Ceil(dBanks).W))
+    val w_serdes = Module(new SerialWidthSlicer(8, rWidth))
+    w_serdes.io.wide <> io.write
 
-    val enq_ptr = RegInit(0.U(log2Ceil(nBanks).W))
-    val deq_ptr = RegInit(0.U(log2Ceil(nBanks).W))
+    // FINE counter tracking the read address
+    val (r_col_count, r_col_done) = Counter(io.read.response.valid, imgWidth)
+    val r_addr = Wire(UInt(log2Ceil(dBanks).W))
+    val r_enable = Vec(nBanks, Bool())
+    val r_datas = VecInit(Seq.fill(nRows){Wire(UInt(rWidth.W))})
+    val (deq_ptr, _) = Counter(r_col_done, nBanks)
 
-    val w_idx = RegInit(0.U(log2Ceil(nBanks).W))
-    val w_addr = RegInit(0.U(log2Ceil(w).W))
-    val r_addr = RegInit(0.U(log2Ceil(w).W)) 
-
+    io.read.response.bits:= VecInit(0 until nRows map (i => (r_datas(deq_ptr + i)%nBanks).U))
+    io.read.request.ready := state === s_stable || state === s_tail
     // Generate NBanks SRAM Banks
     val sram_banks = VecInit(Seq.fill(nBanks){SyncReadMem(dBanks, UInt(rWidth.W))})
     foreach (sram_banks.zipWithIndex) { case (sram, i) =>
-        sram.readWrite()
-    }
-    io.write.ready := valids.reduce(_&&_)
+        r_datas(i) := sram.readWrite(idx       = Mux(w_enable(i), w_addr, r_addr), 
+                                     writeData = w_serdes.io.narrow.bits, 
+                                     en        = w_enable(i) || r_enable(i),
+                                     isWrite   = w_enable(i))}
 
     // state machine
     val s_idle :: s_fill :: s_stable :: s_tail :: Nil = Enum(4)
+
+    val state = RegInit(s_idle)
+    switch(state){
+        // wait for write
+        is(s_idle){
+            state := Mux(io.write.fire, s_fill, s_idle)
+            w_serdes.io.narrow.ready := true.B
+            io.read.response.valid := false.B
+        }
+
+        // fill the banks until nBanks-1 banks are full
+        is(s_fill){
+            state := Mux(enq_ptr === nRows.U, s_stable, s_fill)
+            w_serdes.io.narrow.ready := w_bytes_done
+            w_addr := w_bytes_count + w_col_count << 2
+            io.read.response.valid := false.B
+        }
+
+        // write one bank, waiting for one full row read 
+        is(s_stable){
+            state := Mux(w_row_count === imgHeight.U, s_tail, s_stable)
+            w_serdes.io.narrow.ready := w_bytes_done && !w_col_done
+            for (i <- 0 until nBanks) {
+                r_enable(i) := i.U =/= enq_ptr && !r_col_done && io.read.request.valid
+            }
+            r_addr := r_col_count
+            io.read.response.valid := RegNext(io.read.request.valid && !r_col_done)
+        }
+
+        // tail case, reject writes and only perform reads
+        is(s_tail){
+            w_serdes.io.narrow.ready := false.B
+            state := Mux(r_col_done, s_idle, s_tail)
+            for (i <- 0 until nBanks) {
+                r_enable(i) := i.U =/= enq_ptr && !r_col_done && io.read.request.valid
+            }
+            r_addr := r_col_count
+            io.read.response.valid := RegNext(io.read.request.valid && !r_col_done)
+        }
+    }
 }
 
 
--- src/main/scala/Parameters.scala
+++ src/main/scala/Parameters.scala
@@ -8,7 +8,7 @@ case class RevelioParams(
     blockSize: Int = 8,
     // The number of the functional units
     fuWidth: Int = 4,
-    // The depth of the functional units (number of PEs)
+    // The depth of the functional units (number of blocks computed per cycle)
     fuDepth: Int = 8,
     // The cost function used
     costFunct: String = "SAD",
@@ -19,8 +19,8 @@ case class RevelioParams(
     // Use SRAM for the row buffer
     useSRAM: Boolean = true,
 ){
-    require(fuDepth % blockSize == 0, "The depth of the functional units must be a multiple of the block size")
+    require(fuDepth % 4 == 0, "The depth of the functional units must be a multiple of 4")
     require(Seq("SAD", "SSD", "NCC").contains(costFunction), "The cost function must be one of SAD, SSD, NCC")
-    require(imgWidth % 4 == 0, "The width of the image must be a multiple of 4")
+    require(imgWidth % 4 == 0, "The width of the image must be a multiple of 4bytes, 32 bits")
     def numBlocksPerIter: Int = fuDepth / blockSize
 }
